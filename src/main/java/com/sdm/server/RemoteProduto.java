c946de2b5d772a716046bee357c4650adefd5ee4
Arthur <akposselt@gmail.com>
2025-11-19 00:10:31 -0300

    refactor: mudando List para ArrayList em listar()

--- src/main/java/com/sdm/server/RemoteProduto.java
+++ src/main/java/com/sdm/server/RemoteProduto.java
@@ -1,17 +1,19 @@
 package com.sdm.server;
 
-import com.sdm.model.Produto;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
+import com.sdm.model.Produto;
+import java.util.ArrayList;
 
 public interface RemoteProduto extends Remote {
 
-    List<Produto> listar() throws RemoteException;
-    Produto buscarPorId(int id) throws RemoteException;
-    void inserir(Produto p) throws RemoteException;
-    void atualizar(Produto p) throws RemoteException;
-    boolean deletar(int id) throws RemoteException;
+    ArrayList<Produto> listar() throws RemoteException;
 
+    Produto buscarPorId(int id) throws RemoteException;
+
+    void inserir(Produto p) throws RemoteException;
+
+    void atualizar(Produto p) throws RemoteException;
+
+    boolean deletar(int id) throws RemoteException;
 }
