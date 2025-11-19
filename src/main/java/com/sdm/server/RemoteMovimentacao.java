151de5c3ab9657e98025b1635f6953d650c2ae34
Arthur <akposselt@gmail.com>
2025-11-19 00:09:41 -0300

    feat: adicionando métodos a RemoteMovimentação

--- src/main/java/com/sdm/server/RemoteMovimentacao.java
+++ src/main/java/com/sdm/server/RemoteMovimentacao.java
@@ -1,10 +1,15 @@
 package com.sdm.server;
 
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import com.sdm.model.Movimentacao;
+import java.util.List;
 
 public interface RemoteMovimentacao extends Remote {
+    
+    public boolean inserir(Movimentacao movimentacao) throws RemoteException;
+    
+    public List<Movimentacao> listar() throws RemoteException;
 
     boolean registrarMovimentacao(int produtoId, int quantidade, String tipo, String dataStr) throws RemoteException;
 }
